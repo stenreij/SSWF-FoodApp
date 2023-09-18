49186ac3f6784077adaadf298afe2e7c74546568
stenreij <stenkap0113@gmail.com>
2023-09-18 22:18:01 +0200

    ApplicationServices added/updated
    Hopefully this Application.Services does work

---
+++ Application.Services/IMealPackageService.cs
@@ -0,0 +1,8 @@
+﻿
+namespace Application.Services
+{
+    public class IMealPackageService
+    {
+
+    }
+}
---
+++ Application.Services/MealPackageService.cs
@@ -0,0 +1,20 @@
+﻿using Core.Domain;
+using Core.DomainServices;
+
+namespace Application.Services
+{
+    public class MealPackageService : IMealPackageService
+    {
+        private readonly IMealPackageRepo _mealRepo;
+
+        public MealPackageService(IMealPackageRepo mealPackageRepo)
+        {
+            _mealRepo = mealPackageRepo;
+        }
+
+        //Get MealPackages ordered by pick up date/time (and not yet reserved)
+        public IEnumerable<MealPackage> GetMealPackages()
+            => _mealRepo.GetMealPackages();
+
+    }
+}
