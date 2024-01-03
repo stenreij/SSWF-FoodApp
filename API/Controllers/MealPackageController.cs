346cfb5667ed1e034664f77c424101832c988bf0
stenreij <74412704+stenreij@users.noreply.github.com>
2024-01-03 17:12:48 +0100

    swagger documentation

---
+++ API/Controllers/MealPackageController.cs
@@ -0,0 +1,96 @@
+using Core.Domain;
+using Core.DomainServices.Interfaces;
+using HotChocolate;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MealPackageController : ControllerBase
+    {
+        private readonly IMealPackageRepo _mealPackageRepository;
+
+        public MealPackageController(IMealPackageRepo mealPackageRepository)
+        {
+            _mealPackageRepository = mealPackageRepository;
+        }
+
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<MealPackage>), 200)]
+        [ProducesResponseType(500)]
+        public IActionResult GetMealPackages()
+        {
+            var mealPackages = _mealPackageRepository.GetMealPackages();
+            return Ok(mealPackages);
+        }
+
+        [HttpGet("available")]
+        [ProducesResponseType(typeof(IEnumerable<MealPackage>), 200)]
+        [ProducesResponseType(500)]
+        public IActionResult GetAvailableMealPackages()
+        {
+            var mealPackages = _mealPackageRepository.GetAvailableMealPackages();
+            return Ok(mealPackages);
+        }
+
+        [HttpGet("reserved")]
+        [ProducesResponseType(typeof(IEnumerable<MealPackage>), 200)]
+        [ProducesResponseType(500)]
+        public IActionResult GetReservedMealPackages()
+        {
+            var mealPackages = _mealPackageRepository.GetReservedMealPackages();
+            return Ok(mealPackages);
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(MealPackage), 200)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(500)]
+        [ProducesErrorResponseType(typeof(void))]
+        public IActionResult GetMealPackageById(int id)
+        {
+            try
+            {
+                var mealPackage = _mealPackageRepository.GetMealPackageById(id);
+
+                if (mealPackage == null)
+                {
+                    return NotFound("MealPackage not found");
+                }
+
+                return Ok(mealPackage);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Internal Server Error");
+            }
+        }
+
+        [HttpGet("reserved/student/{id}")]
+        [ProducesResponseType(typeof(MealPackage), 200)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(500)]
+        [ProducesErrorResponseType(typeof(void))]
+        public IActionResult GetReservedByStudentId(int id)
+        {
+            try
+            {
+                var mealPackage = _mealPackageRepository.GetReservedMealPackagesByStudent(id);
+
+                if (mealPackage == null)
+                {
+                    return NotFound("MealPackage not found");
+                }
+
+                return Ok(mealPackage);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Internal Server Error");
+            }
+        }
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,11 +1,28 @@
+using Core.DomainServices.Interfaces;
+using Infrastructure;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using HotChocolate;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddGraphQL();
+builder.Services.AddScoped<IMealPackageRepo, MealPackageEFRepo>();
+
+builder.Services.AddDbContext<FoodAppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDbContext<FoodAppIdentityDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("IdentityConnection")));
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddEntityFrameworkStores<FoodAppIdentityDbContext>();
 
 var app = builder.Build();
 
