aa3f363ca25421fcf731e382328277ab9f2feca5
stenreij <74412704+stenreij@users.noreply.github.com>
2024-10-26 15:51:58 +0200

    api program.cs update

--- API/Program.cs
+++ API/Program.cs
@@ -3,6 +3,7 @@ using Application.Services;
 using Core.DomainServices.Interfaces;
 using Core.DomainServices.Services;
 using Infrastructure;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -22,21 +23,32 @@ builder.Services.AddScoped<IMealPackageService, MealPackageService>();
 
 // Configure database connection strings
 var defaultConnection = string.Empty;
+var identityConnection = string.Empty;
 
 if (builder.Environment.IsDevelopment())
 {
     // Use local connection string during development
     defaultConnection = builder.Configuration.GetConnectionString("LocalDefaultConnection");
+    identityConnection = builder.Configuration.GetConnectionString("IdentityConnection");
+
 }
 else
 {
     // Use environment variable for production connection
     defaultConnection = Environment.GetEnvironmentVariable("DefaultConnection");
+    identityConnection = Environment.GetEnvironmentVariable("IdentityConnection");
 }
 
 // Register DbContext with appropriate connection string
 builder.Services.AddDbContext<FoodAppDbContext>(options =>
     options.UseSqlServer(defaultConnection));
+
+builder.Services.AddDbContext<FoodAppIdentityDbContext>(options =>
+    options.UseSqlServer(identityConnection));
+
+// Add Identity services
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddEntityFrameworkStores<FoodAppIdentityDbContext>();
 
 // Configure GraphQL services
 builder.Services.AddGraphQLServer()
@@ -50,6 +62,15 @@ var app = builder.Build();
 
 app.UseRouting();
 
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+    endpoints.MapGraphQL("/graphql");
+});
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -57,13 +78,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapGraphQL("/graphql");
-});
-
 app.UseHttpsRedirection();
-app.UseAuthorization();
-app.MapControllers();
 
 app.Run();
