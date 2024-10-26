f6492e08f687738b3b306aee7e759ee3f23a8c3c
stenreij <74412704+stenreij@users.noreply.github.com>
2024-10-26 17:30:10 +0200

    development & production

--- API/Program.cs
+++ API/Program.cs
@@ -72,7 +72,7 @@ app.UseEndpoints(endpoints =>
 });
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
