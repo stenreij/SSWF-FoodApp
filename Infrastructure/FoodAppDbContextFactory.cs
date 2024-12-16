f43725bc927fb300ad42a3f0ac5d03acd7155def
stenreij <stenkap0113@gmail.com>
2024-12-16 22:09:09 +0100

    Create FoodAppDbContextFactory.cs
    testing the factory for pipeline

---
+++ Infrastructure/FoodAppDbContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Infrastructure
+{
+    public class FoodAppDbContextFactory : IDesignTimeDbContextFactory<FoodAppDbContext>
+    {
+        public FoodAppDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<FoodAppDbContext>();
+
+            var connectionString = Environment.GetEnvironmentVariable("AZURE_SQL_CONNECTIONSTRINGFOODAPPDB");
+
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new InvalidOperationException("Connection string is not set.");
+            }
+
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new FoodAppDbContext(optionsBuilder.Options);
+        }
+    }
+}
