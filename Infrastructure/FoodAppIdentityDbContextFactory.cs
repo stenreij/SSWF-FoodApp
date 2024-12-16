0b6703cc03e3ce2bfa27a18e35d3eca521582055
stenreij <stenkap0113@gmail.com>
2024-12-16 22:18:39 +0100

    Create FoodAppIdentityDbContextFactory.cs

---
+++ Infrastructure/FoodAppIdentityDbContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Infrastructure
+{
+    public class FoodAppIdentityDbContextFactory : IDesignTimeDbContextFactory<FoodAppIdentityDbContext>
+    {
+        public FoodAppIdentityDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<FoodAppIdentityDbContext>();
+
+            var connectionString = Environment.GetEnvironmentVariable("AZURE_SQL_CONNECTIONSTRINGFOODAPPIDENTITYDB");
+
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new InvalidOperationException("Connection string is not set.");
+            }
+
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new FoodAppIdentityDbContext(optionsBuilder.Options);
+        }
+    }
+}
