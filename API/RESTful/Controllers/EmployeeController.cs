2bc8f0d0c21dac2646d2baa83cb8b6ddfaaca33a
stenreij <74412704+stenreij@users.noreply.github.com>
2024-12-17 16:57:57 +0100

    API get all students / employees

---
+++ API/RESTful/Controllers/EmployeeController.cs
@@ -0,0 +1,43 @@
+﻿using Application.Services;
+using Core.Domain;
+using Core.DomainServices.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Infrastructure;
+using API.RESTful.DTO;
+
+namespace API.RESTful.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeeController : Controller
+    {
+        private readonly IEmployeeRepo _employeeRepo;
+        private readonly ILogger<EmployeeController> _logger;
+
+        public EmployeeController(
+            IEmployeeRepo employeeRepo,
+            ILogger<EmployeeController> logger
+            )
+        {
+            _employeeRepo = employeeRepo;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Student>), 200)]
+        [ProducesResponseType(500)]
+        public IActionResult GetEmployees()
+        {
+            _logger.LogInformation("GetEmployees() called");
+            try
+            {
+                var employees = _employeeRepo.GetEmployees();
+                return Ok(employees);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { Success = false, Error = ex.Message });
+            }
+        }
+    }
+}
---
+++ API/RESTful/Controllers/StudentController.cs
@@ -0,0 +1,43 @@
+﻿using Application.Services;
+using Core.Domain;
+using Core.DomainServices.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Infrastructure;
+using API.RESTful.DTO;
+
+namespace API.RESTful.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StudentController : Controller
+    {
+        private readonly ILogger<StudentController> _logger;
+        private readonly IStudentRepo _studentRepo;
+
+        public StudentController(
+            IStudentRepo studentRepo,
+            ILogger<StudentController> logger
+            )
+        {
+            _studentRepo = studentRepo;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Student>), 200)]
+        [ProducesResponseType(500)]
+        public IActionResult GetStudents()
+        {
+            _logger.LogInformation("GetStudents() called");
+            try
+            {
+                var students = _studentRepo.GetStudents();
+                return Ok(students);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { Success = false, Error = ex.Message });
+            }
+        }
+    }
+}
